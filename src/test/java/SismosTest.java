b1881a6295aafb51804099da06422ab2186fd8be
NandoProjectoFinal <61599328+NandoProjectoFinal@users.noreply.github.com>
2022-04-13 10:51:29 -0400

    Comit Final Juan

--- src/test/java/SismosTest.java
+++ src/test/java/SismosTest.java
@@ -1,5 +1,39 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SismosTest {
+    double[][] arr;
 
+    @BeforeEach
+    public void init() {
+        arr = new double[][]{
+                {2.5, 3.0, 2.7, 3.8, 4.0, 3.1, 4.6, 2.3, 3.2, 3.1, 2.5, 2.2, 2.5, 3.0, 2.7, 3.8, 4.0, 3.1, 4.5, 2.3, 3.2, 3.1, 2.5, 2.2},
+                {4.5, 2.3, 3.2, 3.1, 2.5, 2.2, 2.5, 3.0, 2.7, 3.8, 4.0, 3.1, 2.5, 2.2, 2.5, 3.0, 2.7, 3.8, 2.5, 3.0, 2.7, 3.8, 4.0, 3.1},
+                {2.3, 3.2, 3.1, 2.5, 2.2, 2.5, 2.2, 2.5, 3.0, 2.7, 3.8, 2.5, 3.0, 2.7, 3.8, 4.0, 3.1, 3.8, 4.0, 3.1, 4.5, 2.3, 3.2, 2.9},
+                {2.5, 3.0, 2.7, 3.8, 4.0, 3.1, 4.5, 2.3, 3.2, 3.1, 2.5, 2.2, 2.5, 2.2, 2.5, 3.0, 2.7, 3.8, 2.7, 3.8, 4.5, 2.3, 3.2, 3.1},
+                {3.8, 4.0, 3.1, 4.5, 2.3, 3.2, 2.5, 2.2, 2.5, 3.0, 2.7, 3.8, 4.5, 2.3, 3.2, 3.1, 2.5, 2.2, 2.5, 3.0, 2.7, 3.8, 4.0, 3.1}
+        };
+    }
+
+    @Test
+    public void Test() {
+        Sismos a = new Sismos();
+        double b = a.mayorsismo(arr);
+        assertEquals(b, 2.0);
+    }
+
+    @Test
+    public void Test2() {
+        Sismos a = new Sismos();
+        double b = a.mayorsismo(arr);
+        assertEquals(b, 4.6);
+    }
+
+    @AfterEach
+    void TearDown() {
+        arr = new double[0][0];
+    }
 }
