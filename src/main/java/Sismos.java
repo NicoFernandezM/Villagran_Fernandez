5c0e35cda7d4ce9a384949d6aaa99b6029869d10
NicoFernandezM <n.fernandez07@ufromail.cl>
2022-04-13 09:26:51 -0400

    Primer Commit

---
+++ src/main/java/Sismos.java
@@ -0,0 +1,46 @@
+import java.math.BigDecimal;
+import java.util.FormatFlagsConversionMismatchException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Sismos {
+    public static void main() {
+
+    }
+
+    public static int pedirDias() {
+        int dias;
+
+        while(true) {
+            System.out.println("Por favor ingrese la cantidad de días");
+            Scanner scanner = new Scanner(System.in);
+
+            try {
+                dias = scanner.nextInt();
+            }catch(InputMismatchException e) {
+                continue;
+            }
+
+            if(dias > 1 && dias < 32) {
+                return dias;
+            }
+        }
+    }
+
+    public static double [][] rellenarMatriz(int dias) {
+        double [][] matrizRellena = new double[dias][24];
+        double gradoSismoSinTruncar;
+
+
+        for(int i = 0; i < matrizRellena.length; i++) {
+            for(int j = 0; j < matrizRellena[i].length; j++ ) {
+                gradoSismoSinTruncar = (1 + (Math.random() * 8.6));
+                BigDecimal gradosSismo = new BigDecimal(gradoSismoSinTruncar);
+
+            }
+        }
+        return matrizRellena;
+    }
+
+
+}
